2aec47f5389275531145173c49d6da98419c4676
Hunter <8601934+judge2020@users.noreply.github.com>
2019-07-30 23:03:26 -0400

    Exclude vue from worker tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     rules: [
       {
         loader: 'ts-loader',
-        exclude: [/node_modules/],
+        exclude: [/node_modules/, /src/, /outfrontend/],
         options: {
           configFile: 'worker.tsconfig.json',
         },
