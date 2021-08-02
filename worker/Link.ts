b8639de77e8894008a839026ea782949126a9b55
Hunter Ray <8601934+judge2020@users.noreply.github.com>
2021-08-02 00:25:46 -0400

    Core redirection and analytics logic
    Closes https://github.com/judge2020/workers-link-shortener/issues/22

--- worker/Link.ts
+++ worker/Link.ts
@@ -1,8 +1,13 @@
+// Link.ts
+// only for analytics for now
+
 export class Link {
     state: DurableObjectState;
     env: Env;
 
     hits = 0;
+    countries: any;
+    datetimes: any;
     //target: string = '';
 
     initializePromise: Promise<void> | undefined
@@ -12,32 +17,54 @@ export class Link {
         this.state = state;
         this.env = env;
     }
+
     async initialize() {
         let _hits = await this.state.storage.get<number>("hits");
         this.hits = _hits || 0;
-        //let _target = await this.state.storage.get("target");
-        //this.target = _target;
+        let _countries = await this.state.storage.get<any>("countries");
+        this.countries = _countries || {};
+        let _datetimes = await this.state.storage.get<any>("datetimes");
+        this.datetimes = _datetimes || [];
     }
 
     async fetch(request: Request) {
         if (!this.initializePromise) {
             this.initializePromise = this.initialize().catch((err) => {
-                // If anything throws during initialization then we need to be
-                // sure that a future request will retry initialize().
-                // Note that the concurrency involved in resetting this shared
-                // promise on an error can be tricky to get right -- we don't
-                // recommend customizing it.
                 this.initializePromise = undefined;
                 throw err
             });
         }
         await this.initializePromise;
-        //let newHits = this.hits + 1;
-        //this.hits = newHits;
-        //await this.state.storage.put("hits", newHits);
+
+        let url = new URL(request.url);
+
+        if (url.pathname == '/increment') {
+            this.hits += 1;
+            await this.state.storage.put("hits", this.hits);
+
+            //country tracking increment logic
+            let country = url.searchParams.get('country')!;
+
+            // eslint-disable-next-line no-prototype-builtins
+            if (this.countries.hasOwnProperty(country)) {
+                this.countries[country] += 1;
+            }
+            else {
+                this.countries[country] = 1;
+            }
+            await this.state.storage.put("countries", this.countries);
+
+            //date time tracking logic
+            // list of ISO date elements won't use much data - you'd need hundreds of millions of hits to get to 10gb
+            let datetime = (new Date).toISOString().split('.')[0]+"Z"
+
+            this.datetimes.push(datetime);
+            await this.state.storage.put("datetimes", this.datetimes);
+
+            return new Response(`Shortcode now has ${this.countries["US"]} US hits and ${this.hits} in total with hits at ${JSON.stringify(this.datetimes)}.`)
+        }
 
         return new Response(JSON.stringify(request));
-
     }
 }
 
--- worker/index.ts
+++ worker/index.ts
@@ -9,11 +9,44 @@ export default {
 }
 
 async function handleRequest(request: Request, env: Env) {
-  // todo logic https://github.com/judge2020/workers-link-shortener/issues/22
-  let id = env.LINK.idFromName("A");
-  let obj = env.LINK.get(id);
-  let resp = await obj.fetch(request);
-  let count = await resp.text();
-
-  return new Response(count);
+  let url = new URL(request.url);
+  // remove leading slash
+  let reqPath = url.pathname.substring(1);
+  // default to the key `_index` for pulling the index redirect
+  if (reqPath == '') {
+    reqPath = '_index';
+  }
+  // handle creating via temporary API.
+  if (reqPath == 'create') {
+    let shortcode = url.searchParams.get("shortcode")!;
+    let target = url.searchParams.get("target")!;
+    let overwrite = url.searchParams.get("overwrite")!;
+    if (!shortcode || !target) {
+      return new Response("Please pass shortcode and target")
+    }
+    try {
+      new URL(target!);
+    }
+    catch (TypeError) {
+      return new Response(`Not a valid URL ${target}`)
+    }
+    if (!overwrite && (await env.WLS_LINK_LIST.get(shortcode)) != undefined) {
+      // TODO: option to not generate new analytics durable object
+      return new Response(`Shortcode ${shortcode} already exists. Use &overwrite=yes to overwrite it. This will also reset analytics.`)
+    }
+    let new_analytics_id = env.LINK.newUniqueId().toString();
+    await env.WLS_LINK_LIST.put(shortcode, JSON.stringify({
+      target: target,
+      do_analytics_id: new_analytics_id
+    }))
+    return new Response(`created shortcode ${shortcode} with target ${target}`)
+  }
+  let potential_short = await env.WLS_LINK_LIST.get<any>(reqPath, "json");
+  if (!potential_short) {
+    return new Response(`404: ${reqPath} not found`, {status: 404});
+  }
+  let analytics_stub = await env.LINK.get(potential_short.do_analytics_id);
+  // to show the response of the Durable Object during development, just prepend `return` to the next line.
+  return await analytics_stub.fetch(`https://worker.local/increment?country=${request.cf.country}`);
+  return new Response('', {headers: {"location": potential_short.target}, status: 302});
 }
